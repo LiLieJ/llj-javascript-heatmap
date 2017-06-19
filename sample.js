e3d9c01594dc536b04e6ef1be6cce8c5d276d842
unknown <>
2017-06-19 23:27:13 +0900

    second commit!

---
+++ sample.js
@@ -0,0 +1,28 @@
+function generateRandomData(len){
+	var points=[];
+	var max=0;
+	var width=840;
+	var height=400;
+	while(len--){
+		var val=Math.floor(Math.random()*100);
+		max=Math.max(max,val);
+		var point={
+			x:Math.floor(Math.random()*width),
+			y:Math.floor(Math.random()*height),
+			value:val
+		};
+		points.push(point);
+	}
+	var data={max:max,data:points};
+	return data;
+}
+
+function run(){
+	var heatmapInstance=h337.create({
+		container:document.querySelector('.heatmap')
+		});
+
+	var data=generateRandomData(200);
+
+	heatmapInstance.setData(data);
+}
